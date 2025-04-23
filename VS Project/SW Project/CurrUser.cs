8d685a826703a6040393f334c219d5bc4602826c
mazen <mazen15408@gmail.com>
2025-04-23 17:45:26 +0200

    login page done using disconnected mode

---
+++ VS Project/SW Project/CurrUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW_Project
+{
+    internal static class CurrUser
+    {
+        
+        public static string Username { get; set; }
+        public static int UserId { get; set; }
+        public static Boolean IsAdmin { get; set; }
+        public static void Logout()
+        {
+            Username = null;
+            UserId = 0;
+        }
+        public static void LogIn(string username, int userId, bool isAdmin)
+        {
+            Username = username;
+            UserId = userId;
+            IsAdmin = isAdmin;
+        }
+    }
+}
