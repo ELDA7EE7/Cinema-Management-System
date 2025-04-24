edd29eaf08f287005c3c1ed00c4c303c956b46f4
mazen <mazen15408@gmail.com>
2025-04-24 02:08:50 +0200

    make powerful ui for all pages

--- VS Project/SW Project/AddAdmin.Designer.cs
+++ VS Project/SW Project/AddAdmin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAdmin));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,119 +40,174 @@
             this.phone_tb = new System.Windows.Forms.TextBox();
             this.age_tb = new System.Windows.Forms.TextBox();
             this.add_button = new System.Windows.Forms.Button();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelForm = new System.Windows.Forms.Panel();
+            this.panelHeader.SuspendLayout();
+            this.panelForm.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 109);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Username";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(159, 150);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Email";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(159, 191);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Password";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(160, 230);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Phone Number";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(162, 273);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(26, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Age";
-            // 
-            // username_tb
-            // 
-            this.username_tb.Location = new System.Drawing.Point(337, 101);
-            this.username_tb.Name = "username_tb";
-            this.username_tb.Size = new System.Drawing.Size(100, 20);
-            this.username_tb.TabIndex = 6;
-            // 
-            // email_tb
-            // 
-            this.email_tb.Location = new System.Drawing.Point(337, 142);
-            this.email_tb.Name = "email_tb";
-            this.email_tb.Size = new System.Drawing.Size(100, 20);
-            this.email_tb.TabIndex = 7;
-            // 
-            // password_tb
-            // 
-            this.password_tb.Location = new System.Drawing.Point(337, 183);
-            this.password_tb.Name = "password_tb";
-            this.password_tb.Size = new System.Drawing.Size(100, 20);
-            this.password_tb.TabIndex = 8;
-            // 
-            // phone_tb
-            // 
-            this.phone_tb.Location = new System.Drawing.Point(337, 222);
-            this.phone_tb.Name = "phone_tb";
-            this.phone_tb.Size = new System.Drawing.Size(100, 20);
-            this.phone_tb.TabIndex = 9;
-            // 
-            // age_tb
-            // 
-            this.age_tb.Location = new System.Drawing.Point(337, 265);
-            this.age_tb.Name = "age_tb";
-            this.age_tb.Size = new System.Drawing.Size(100, 20);
-            this.age_tb.TabIndex = 10;
-            // 
-            // add_button
-            // 
-            this.add_button.Location = new System.Drawing.Point(337, 355);
-            this.add_button.Name = "add_button";
-            this.add_button.Size = new System.Drawing.Size(75, 23);
-            this.add_button.TabIndex = 11;
-            this.add_button.Text = "Add";
-            this.add_button.UseVisualStyleBackColor = true;
-            this.add_button.Click += new System.EventHandler(this.add_button_Click);
-            // 
-            // AddAdmin
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.add_button);
-            this.Controls.Add(this.age_tb);
-            this.Controls.Add(this.phone_tb);
-            this.Controls.Add(this.password_tb);
-            this.Controls.Add(this.email_tb);
-            this.Controls.Add(this.username_tb);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "AddAdmin";
-            this.Text = "Add Admin";
-            this.ResumeLayout(false);
-            this.PerformLayout();
 
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(30, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email:";
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(30, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+
+            // label4
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(30, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phone Number:";
+
+            // label5
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(30, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Age:";
+
+            // username_tb
+            this.username_tb.BackColor = System.Drawing.Color.White;
+            this.username_tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.username_tb.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.username_tb.Location = new System.Drawing.Point(180, 30);
+            this.username_tb.Name = "username_tb";
+            this.username_tb.Size = new System.Drawing.Size(250, 25);
+            this.username_tb.TabIndex = 6;
+
+            // email_tb
+            this.email_tb.BackColor = System.Drawing.Color.White;
+            this.email_tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.email_tb.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.email_tb.Location = new System.Drawing.Point(180, 80);
+            this.email_tb.Name = "email_tb";
+            this.email_tb.Size = new System.Drawing.Size(250, 25);
+            this.email_tb.TabIndex = 7;
+
+            // password_tb
+            this.password_tb.BackColor = System.Drawing.Color.White;
+            this.password_tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.password_tb.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.password_tb.Location = new System.Drawing.Point(180, 130);
+            this.password_tb.Name = "password_tb";
+            this.password_tb.Size = new System.Drawing.Size(250, 25);
+            this.password_tb.TabIndex = 8;
+            this.password_tb.UseSystemPasswordChar = true;
+
+            // phone_tb
+            this.phone_tb.BackColor = System.Drawing.Color.White;
+            this.phone_tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phone_tb.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.phone_tb.Location = new System.Drawing.Point(180, 180);
+            this.phone_tb.Name = "phone_tb";
+            this.phone_tb.Size = new System.Drawing.Size(250, 25);
+            this.phone_tb.TabIndex = 9;
+
+            // age_tb
+            this.age_tb.BackColor = System.Drawing.Color.White;
+            this.age_tb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.age_tb.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.age_tb.Location = new System.Drawing.Point(180, 230);
+            this.age_tb.Name = "age_tb";
+            this.age_tb.Size = new System.Drawing.Size(250, 25);
+            this.age_tb.TabIndex = 10;
+
+            // add_button
+            this.add_button.BackColor = System.Drawing.Color.SeaGreen;
+            this.add_button.FlatAppearance.BorderSize = 0;
+            this.add_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_button.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.add_button.ForeColor = System.Drawing.Color.White;
+            this.add_button.Location = new System.Drawing.Point(180, 290);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(250, 40);
+            this.add_button.TabIndex = 11;
+            this.add_button.Text = "Add Admin";
+            this.add_button.UseVisualStyleBackColor = false;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+
+            // panelHeader
+            this.panelHeader.BackColor = System.Drawing.Color.SteelBlue;
+            this.panelHeader.Controls.Add(this.lblTitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(500, 70);
+            this.panelHeader.TabIndex = 12;
+
+            // lblTitle
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(127, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Add Admin";
+
+            // panelForm
+            this.panelForm.BackColor = System.Drawing.Color.White;
+            this.panelForm.Controls.Add(this.label1);
+            this.panelForm.Controls.Add(this.username_tb);
+            this.panelForm.Controls.Add(this.add_button);
+            this.panelForm.Controls.Add(this.label2);
+            this.panelForm.Controls.Add(this.age_tb);
+            this.panelForm.Controls.Add(this.email_tb);
+            this.panelForm.Controls.Add(this.phone_tb);
+            this.panelForm.Controls.Add(this.label3);
+            this.panelForm.Controls.Add(this.password_tb);
+            this.panelForm.Controls.Add(this.label4);
+            this.panelForm.Controls.Add(this.label5);
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelForm.Location = new System.Drawing.Point(0, 70);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Padding = new System.Windows.Forms.Padding(20);
+            this.panelForm.Size = new System.Drawing.Size(500, 380);
+            this.panelForm.TabIndex = 13;
+
+            // AddAdmin
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.panelForm);
+            this.Controls.Add(this.panelHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Admin";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelForm.ResumeLayout(false);
+            this.panelForm.PerformLayout();
+            this.ResumeLayout(false);
         }
 
         #endregion
@@ -166,5 +222,8 @@
         private System.Windows.Forms.TextBox phone_tb;
         private System.Windows.Forms.TextBox age_tb;
         private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelForm;
     }
 }
